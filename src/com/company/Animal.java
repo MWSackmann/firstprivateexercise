abe3f16e99b9b878f042c7ae939370c4a1541002
Michael Sackmann <MWSackmann@gmx.de>
2016-04-17 18:10:36 +0200

    add some if/case operations

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -54,6 +54,7 @@ public class Animal {
         }
         this.setUniqueID();
 
+        this.someOperations();
        }
 
     public static void main(String[] args){
@@ -105,12 +106,15 @@ public class Animal {
     public void setUniqueID() {
         long minNumber = 1;
         long maxNumber = 1000000;
-        //long is used for casting as Math.random returns double
+        //(long) is used for casting as Math.random returns double
         this.uniqueID = minNumber + (long) (Math.random() * ((maxNumber - minNumber) +1 ) );
 
         // conversion
-        String stringNumber = Long.toString(maxNumber);
+        String stringNumber = Long.toString(this.uniqueID);
+        System.out.println(stringNumber);
+
         int numberString = Integer.parseInt(stringNumber);
+        System.out.println(numberString);
     }
 
     public char getFavoriteChar() {
@@ -135,5 +139,38 @@ public class Animal {
 
     public void setHeight(float height) {
         this.height = height;
+    }
+
+    public void someOperations(){
+
+        int randomInt = (int) (Math.random() + 10);
+        System.out.println("Random Int: " + randomInt);
+
+        //  &&  and
+        //  ||  or
+        //  !   not (return opposite for boolean !true returns false
+        if (randomInt > 10 && randomInt < 100){
+            System.out.println("if statment = true");
+        } else {
+            System.out.println("if statement = false");
+        }
+
+        // if randdomInt is smaller then 50 assign 50, otherwise assign randomInt
+        int whichIsBigger = (50 > randomInt) ? 50 : randomInt;
+        System.out.println(whichIsBigger);
+
+        switch (randomInt){
+            case 0:
+                System.out.println("integer has initial value");
+                break;
+            case 10:
+            case 11:
+            case 12:
+                System.out.println("integer is either 10, 11 or 12");
+                break;
+            default:   // like when others
+                System.out.println("integer has some other value");
+                break;
+        }
     }
 }
